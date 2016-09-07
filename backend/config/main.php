509f4ff4fd03f88c5b9130661ed3c77dcbcab012
yaoyang <yaoyang@mobvoi.com>
2016-09-07 16:40:18 +0800

    Merge branch 'wf-dev' into 'master'
    Wf dev
    
    See merge request !1 (reverted from commit 58c3a2f08010d250dcf1aec1191731cf472db84a)

--- backend/config/main.php
+++ backend/config/main.php
@@ -11,20 +11,16 @@ return [
     'basePath' => dirname(__DIR__),
     'controllerNamespace' => 'backend\controllers',
     'bootstrap' => ['log'],
-    'modules' => [
-            'user' => [
-                'class' => 'dektrium\user\Module',
-            ],
-    ],
+    'modules' => [],
     'components' => [
         'request' => [
             'csrfParam' => '_csrf-backend',
         ],
-        // 'user' => [
-        //     'identityClass' => 'common\models\User',
-        //     'enableAutoLogin' => true,
-        //     'identityCookie' => ['name' => '_identity-backend', 'httpOnly' => true],
-        // ],
+        'user' => [
+            'identityClass' => 'common\models\User',
+            'enableAutoLogin' => true,
+            'identityCookie' => ['name' => '_identity-backend', 'httpOnly' => true],
+        ],
         'session' => [
             // this is the name of the session cookie used for login on the backend
             'name' => 'advanced-backend',
--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -36,17 +36,12 @@ AppAsset::register($this);
     ]);
     $menuItems = [
         ['label' => 'Home', 'url' => ['/site/index']],
-        ['label' => '审核表', 'url' => ['/form-info/index']],
-        ['label' => '一级问题表', 'url' => ['/fp/index']],
-        ['label' => '二级问题表', 'url' => ['/sp/index']],
-        ['label' => '问题检查表', 'url' => ['/check/index']],
-        ['label' => 'Lookup', 'url' => ['/lookup/index']],
     ];
     if (Yii::$app->user->isGuest) {
-        $menuItems[] = ['label' => 'Login', 'url' => ['/user/security/login']];
+        $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
     } else {
         $menuItems[] = '<li>'
-            . Html::beginForm(['/user/security/logout'], 'post')
+            . Html::beginForm(['/site/logout'], 'post')
             . Html::submitButton(
                 'Logout (' . Yii::$app->user->identity->username . ')',
                 ['class' => 'btn btn-link']
