409014ed75d475f5e202148bcdcb8dc528f495ef
wfzhang <wfzhang@mobvoi.com>
2016-09-08 15:19:39 +0800

    fix

--- frontend/controllers/InfoController.php
+++ frontend/controllers/InfoController.php
@@ -3,8 +3,6 @@
 namespace frontend\controllers;
 
 use Yii;
-use backend\models\Info;
-use backend\models\InfoSearch;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
@@ -44,101 +42,6 @@ class InfoController extends Controller
      */
     public function actionIndex()
     {
-        $searchModel = new InfoSearch();
-        $param = Yii::$app->request->queryParams;
-        $param['InfoSearch']['user_id'] = Yii::$app->user->id;
-//         print_r($param);
-        $dataProvider = $searchModel->search($param);
-
-        return $this->render('index', [
-            'searchModel' => $searchModel,
-            'dataProvider' => $dataProvider,
-        ]);
-    }
-
-    /**
-     * Displays a single Info model.
-     * @param integer $id
-     * @return mixed
-     */
-    public function actionView($id)
-    {
-        return $this->render('view', [
-            'model' => $this->findModel($id),
-        ]);
-    }
-
-    /**
-     * Creates a new Info model.
-     * If creation is successful, the browser will be redirected to the 'view' page.
-     * @return mixed
-     */
-    public function actionCreate()
-    {
-        $model = new Info();
-
-        if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['view', 'id' => $model->id]);
-        } else {
-            return $this->render('create', [
-                'model' => $model,
-            ]);
-        }
-    }
-
-    /**
-     * Updates an existing Info model.
-     * If update is successful, the browser will be redirected to the 'view' page.
-     * @param integer $id
-     * @return mixed
-     */
-    public function actionUpdate($id)
-    {
-        $model = $this->findModel($id);
-
-        if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['view', 'id' => $model->id]);
-        } else {
-            return $this->render('update', [
-                'model' => $model,
-            ]);
-        }
-    }
-
-    /**
-     * Deletes an existing Info model.
-     * If deletion is successful, the browser will be redirected to the 'index' page.
-     * @param integer $id
-     * @return mixed
-     */
-    public function actionDelete($id)
-    {
-        $model = Info::find()->where(['id'=>$id])
-        ->andWhere(['status' => 1])
-        ->andWhere(['user_id' => yii::$app->user->id])
-        ->one();
-        if ($model !== null) {
-        	$model->delete();
-        } else {
-        	throw new NotFoundHttpException('The requested page does not exist.');
-        }
-        
-        return $this->redirect(['index']);
-    }
-
-    /**
-     * Finds the Info model based on its primary key value.
-     * If the model is not found, a 404 HTTP exception will be thrown.
-     * @param integer $id
-     * @return Info the loaded model
-     * @throws NotFoundHttpException if the model cannot be found
-     */
-    protected function findModel($id)
-    {
-        if (($model = Info::findOne(['id' => $id, 'user_id' => yii::$app->user->id])) !== null) {
-            return $model;
-        } else {
-            throw new NotFoundHttpException('The requested page does not exist.');
-        }
+        return $this->render('index');
     }
 }
