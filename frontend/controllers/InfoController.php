123d62f5b4921a7a0010b762706c7d1cdfdbf337
wfzhang <wfzhang@mobvoi.com>
2016-09-10 11:42:21 +0800

    fix

--- frontend/controllers/InfoController.php
+++ frontend/controllers/InfoController.php
@@ -8,6 +8,10 @@ use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
 use yii\filters\AccessControl;
 use backend\models\Fp;
+use backend\models\Sp;
+use backend\models\Check;
+use backend\models\Country;
+use backend\models\FormInfo;
 /**
  * InfoController implements the CRUD actions for Info model.
  */
@@ -24,7 +28,7 @@ class InfoController extends Controller
 					'rules' => [
 							[
 									'allow' => true,
-									'roles' => ['@'],
+									//'roles' => ['@'],
 							],
 					],
 			],
@@ -43,7 +47,60 @@ class InfoController extends Controller
      */
     public function actionIndex()
     {
+        $fp = Fp::find()
+        ->orderBy('position')
+        ->all();
+        $country = Country::find()
+        ->orderBy('position')
+        ->all();
+        return $this->render('index',['fp' => $fp,'country' => $country]);
+    }
 
-        return $this->render('index');
+    public function actionSearch()
+    {
+        $post = Yii::$app->request->post();
+        $fpid = $post['fpid'];
+        $fp = fp::find()
+        ->where(['id' => $fpid])
+        ->one();
+        $list="";
+        if($fp->des=="Others"){
+            return $list;
+        }
+        else{
+            $Sp = Sp::find()
+            ->where(['fpid' => $fpid])
+            ->orderBy('position')
+            ->all();
+            foreach ($Sp as $sp) {
+                $list.="<span class='list-span'><input type='radio' class='sp-btn' name='sp' value='".$sp->id."'>".$sp->des."</span>";
+            }
+            return $list;
+        }
+    }
+    public function actionSelfCheck(){
+        $post = Yii::$app->request->post();
+        $spid = $post['spid'];
+        $check = Check::find()
+        ->where(['spid' => $spid])
+        ->orderBy('position')
+        ->all();
+        $list = '';
+        foreach ($check as $check) {
+            $list.="<p>".$check->des."</p>";
+        }
+        return $list;
+    }
+
+    public function actionInfoInsert(){
+        $model = new FormInfo();
+        echo "2222";
+        if($model->load(Yii::$app->request->post())) {
+            echo "1111";
+            if($model->save()){
+                return "success";
+            }
+        }
+        return "error";
     }
 }
