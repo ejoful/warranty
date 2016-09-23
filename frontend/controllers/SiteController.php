5bc881c37222cd8e759e8e39236610d888c694ad
wfzhang <wfzhang@mobvoi.com>
2016-09-23 15:36:50 +0800

    fix

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -517,11 +517,11 @@ class SiteController extends Controller
      */
     public function actionRequestPasswordReset()
     {
+        echo 123;
         $model = new PasswordResetRequestForm();
         if ($model->load(Yii::$app->request->post()) && $model->validate()) {
             if ($model->sendEmail()) {
                 Yii::$app->session->setFlash('success', 'Check your email for further instructions.');
-
                 return $this->goHome();
             } else {
                 Yii::$app->session->setFlash('error', 'Sorry, we are unable to reset password for email provided.');
